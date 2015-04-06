b5938760d0aa0f38623b522e920465c7115c4fe7
Naik <naik.srinu@gmail.com>
2015-04-06 06:58:56 +0530

    Update smartpcapreplay.py

--- smartpcapreplay.py
+++ smartpcapreplay.py
@@ -11,4 +11,34 @@ naik@sec$ sudo pyhon smartpcapreplay.py mypacket_2gb.pcap -c eth0 -s eth1 -cmac 
           
 '''
 
+def usage():
+        print "##################################################################################################"
+        print "               Smart PCAP Replay Utility with TCP Sessions"
+        print "               Acts as if Client-Server is commuted"
+        print "##################################################################################################"
+        print "Usage : python smartpcapreplay.py -p tcp_mix.pcap -i 00:1d:xx:xx:xx:xx -o 10:1d:xx:xx:xx:xx"
+        print "         -c 10.10.10.2 -s 20.20.20.2"
+        print "-h --> help / Usage guidelines"
+        print "-p --> PCAP to replay"
+        print "-i --> DUT incoming Interface MAC Address"
+        print "-o --> DUT outgoing Interface MAC Address"
+        print "-c --> Client IP"
+        print "-s --> Server IP/Destination IP"
+        
+        print " Target --> SNORT / HoneyPot / Firewall / WAF / Suricata / IPTables / IoT Gateway
+        
+        print " ~~~~~~~~~~~~~~~~~~TestBed~~~~~~~~~~~~~~~~~~~~~~~~~"
+        print "                 |-------------|                   "
+        print "         --------|   Target    |---------|         "
+        print "         |   Eth1|-------------|Eth2     |         "
+        print "         |                               |         "
+        print "         |                               |         "
+        print "         |       |----------|            |         "
+        print "         |-------|   TM     |------------|         "
+        print "               E0|----------|E1                    "
+        print " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        print "##################################################################################################"
+        print "##################################################################################################"
+        sys.exit(0)
+
 
